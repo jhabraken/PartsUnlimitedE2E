dd2b405e543dd9b8d849fafbff63bff70825cf7d
jhabraken <j.m.habraken@hhs.nl>
2020-02-13 15:57:47 +0100

    Commite changes to CarItem.cs file

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        //My second attempt to commit a change.
         [Key]
         public int CartItemId { get; set; }
 
